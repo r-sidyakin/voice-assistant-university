b61b8c7e53643b06fcde7e69e5644a681c4fe700
Serafimka2705 <75986799+Serafimka2705@users.noreply.github.com>
2021-06-07 20:08:29 +0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt install vlc
+sudo apt install galculator
+sudo apt install python3.6
+sudo apt-get install python-pip
+pip3 install python-vlc
+sudo apt-get install portaudio19-dev python-pyaudio
+pip3 install PyAudio
+pip3 install PySide2
+pip3 install SpeechRecognition
