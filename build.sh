cdff6bacc4edc6a12525789a7985c87c72c929fe
Serafimka2705 <75986799+Serafimka2705@users.noreply.github.com>
2021-06-07 20:12:10 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ sudo apt-get install portaudio19-dev python-pyaudio
 pip3 install PyAudio
 pip3 install PySide2
 pip3 install SpeechRecognition
+pip3 install pypiwin32
+pip3 install pyinstaller
+pyinstaller main.py --onefile --noconsole
